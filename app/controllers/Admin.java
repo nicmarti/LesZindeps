49b4dd1bb931c98a0eca38a3f7226053cdd132f8
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2010-12-29 12:29:18 +0100

    Added Yahoo! as an open id provider

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -20,6 +20,8 @@ public class Admin extends Controller {
 
     @Before(unless = {"index",
             "authenticateOpenIdGoogle",
+            "authenticateOpenIdYahoo",
+            "authenticateOpenId",
             "showMyProfile",
             "doUpdateMyProfile"
 
@@ -83,6 +85,102 @@ public class Admin extends Controller {
     }
 
     /**
+     * Réalise l'authentification
+     */
+    public static void authenticateOpenIdYahoo() {
+        if (OpenID.isAuthenticationResponse()) {
+            OpenID.UserInfo verifiedUser = OpenID.getVerifiedID();
+            if (verifiedUser == null) {
+                flash.error("Erreur OpenID Yahoo");
+                index();
+            }
+
+            String userEmail = verifiedUser.extensions.get("email");
+            if (userEmail == null) {
+                flash.error("L'identification de votre compte sur le site des Zindeps s'effectue avec votre email." +
+                        " Vous devez authoriser le domaine leszindeps.fr à accéder à votre email pour vous authentifier."
+                );
+                index();
+            }
+
+            Zindep zindep = Zindep.findByMail(userEmail);
+            if (zindep == null) {
+                flash.error("Désolé votre compte n'existe pas. Demandez à l'équipe d'ajouter votre email "
+                        + userEmail
+                        + " pour pouvoir vous authentifier avec ce compte.");
+                index();
+            }
+            session.put("zindepId", zindep.id);
+            session.put("zindepEmail", zindep.email);
+
+            flash.success("Bienvenue " + zindep.firstName);
+
+            // Attention ne pas passer de parametre ici pr des raisons de securité
+            showMyProfile();
+
+        } else {
+            // Verify the id
+            if (!OpenID.id("http://yahoo.com/").required("email", "http://axschema.org/contact/email").verify()) {
+                // Il faut que vous soyez en ligne sur Internet et pas en localhost !
+                flash.put("error", "Impossible de s'authentifier au service Yahoo!");
+                index();
+            }
+        }
+    }
+
+
+    /**
+     * Réalise l'authentification
+     */
+    public static void authenticateOpenId(String t) {
+        if (OpenID.isAuthenticationResponse()) {
+            OpenID.UserInfo verifiedUser = OpenID.getVerifiedID();
+            if (verifiedUser == null) {
+                flash.error("Erreur OpenID generique");
+                index();
+            }
+
+            String userEmail = verifiedUser.extensions.get("email");
+            if (userEmail == null) {
+                flash.error("L'identification de votre compte sur le site des Zindeps s'effectue avec votre email." +
+                        " Vous devez authoriser le domaine leszindeps.fr à accéder à votre email pour vous authentifier."
+                );
+                index();
+            }
+
+            Zindep zindep = Zindep.findByMail(userEmail);
+            if (zindep == null) {
+                flash.error("Désolé votre compte n'existe pas. Demandez à l'équipe d'ajouter votre email "
+                        + userEmail
+                        + " pour pouvoir vous authentifier avec ce compte.");
+                index();
+            }
+            session.put("zindepId", zindep.id);
+            session.put("zindepEmail", zindep.email);
+
+            flash.success("Bienvenue " + zindep.firstName);
+
+            // Attention ne pas passer de parametre ici pr des raisons de securité
+            showMyProfile();
+
+        } else {
+            if (t == null) {
+                index();
+            }
+            if (t.trim().isEmpty()) {
+                index();
+            }
+
+            // Verify the id
+            if (!OpenID.id(t).required("email", "http://axschema.org/contact/email").verify()) {
+                flash.put("error", "Impossible de s'authentifier avec l'URL utilisée.");
+                index();
+            }
+        }
+    }
+
+
+    /**
      * Cette methode est appelé d'authenticateOpenIdGoogle mais elle peut
      * aussi etre appelée directement si je garde en bookmark cette ressource.
      * Play! Framework est completement sans etat, donc on cherche le zindepId
@@ -95,7 +193,7 @@ public class Admin extends Controller {
         }
 
         Zindep zindep = Zindep.findById(id);
-        if(zindep==null){
+        if (zindep == null) {
             error("Zindep non trouvé");
         }
 
