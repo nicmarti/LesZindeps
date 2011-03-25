276cf5c1cf1d06f98ca90600c5837e7889c477e4
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2011-03-25 18:23:13 +0100

    Ajout d'un message si la partie JS/Ajax ne marche pas

--- app/models/Mission.java
+++ app/models/Mission.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright(c) 2010 Les Zindeps.
+ *
+ * The code source of this project is distributed
+ * under the Affero GPL GNU AFFERO GENERAL PUBLIC LICENSE
+ * Version 3, 19 November 2007
+ *
+ * This file is part of project LesZindeps. The source code is
+ * hosted on GitHub. The initial project was launched by
+ * Nicolas Martignole.
+ *
+ * LesZindeps is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LesZindeps is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *
+ * Please see COPYING.AGPL.txt for the full text license
+ * or online http://www.gnu.org/licenses/agpl.html
+ */
+
 package models;
 
 import java.util.Date;
@@ -62,7 +88,7 @@ public class Mission extends Model
     @Required
     public String location;
     
-    @Required
+    @Required(message = "Le champ expérience est obligatoire, merci d'indiquer un nombre d'années")
     public Long exp;
     
     
