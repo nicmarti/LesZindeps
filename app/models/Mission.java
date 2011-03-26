95aa87c293488b309f6d36cb75357cd02038cea4
Nicolas MARTIGNOLE <nicolas@touilleur-express.fr>
2011-03-26 12:14:48 +0100

    Reformatage à la sauce SUN du fichier Java

--- app/models/Mission.java
+++ app/models/Mission.java
@@ -48,50 +48,47 @@ import play.db.jpa.JPA;
 import play.db.jpa.Model;
 
 /**
- * 
  * Correspond au anciennes missions réalisées par les Zindeps
- * 
  */
 @Entity
-public class Mission extends Model 
-{
+public class Mission extends Model {
     @ManyToOne
     public Zindep zindep;
-    
+
     @Required
     public String title;
-    
+
     @Temporal(TemporalType.DATE)
     @Required
     public Date initialDate;
-    
+
     @Temporal(TemporalType.DATE)
     @Required
     public Date endDate;
-    
+
     @Required
     @Min(0)
     public Long clientPrice;
-    
+
     @Min(0)
     public Long intermediaryPrice;
-    
+
     @Required
     public String customer;
-    
+
     public String intermediary;
-    
+
     @Lob
     @MaxSize(10000)
     public String comment;
-    
+
     @Required
     public String location;
-    
+
     @Required(message = "Le champ expérience est obligatoire, merci d'indiquer un nombre d'années")
     public Long exp;
-    
-    
+
+
     /**
      * Retourne la liste des missions déjà effectuées par cet indep
      *
@@ -100,37 +97,31 @@ public class Mission extends Model
     public static List<Mission> findByZindep(Zindep zindep) {
         return Mission.find("from Mission where zindep = :zindep order by endDate").bind("zindep", zindep).fetch();
     }
-    
-    public static List<String> getListOfIntermediaries(String term)
-    {
+
+    public static List<String> getListOfIntermediaries(String term) {
         String whereClause = "";
-        if (term != null)
-        {
-            whereClause = "where UPPER(intermediary) like UPPER('%"+term+"%') ";
+        if (term != null) {
+            whereClause = "where UPPER(intermediary) like UPPER('%" + term + "%') ";
         }
-        Query query = JPA.em().createQuery("select distinct intermediary from Mission m "+whereClause+" order by intermediary");
+        Query query = JPA.em().createQuery("select distinct intermediary from Mission m " + whereClause + " order by intermediary");
         return query.getResultList();
     }
-    
-    public static List<String> getListOfLocations(String term)
-    {
+
+    public static List<String> getListOfLocations(String term) {
         String whereClause = "";
-        if (term != null)
-        {
-            whereClause = "where UPPER(location) like UPPER('%"+term+"%') ";
+        if (term != null) {
+            whereClause = "where UPPER(location) like UPPER('%" + term + "%') ";
         }
-        Query query = JPA.em().createQuery("select distinct location from Mission m "+whereClause+" order by location");
+        Query query = JPA.em().createQuery("select distinct location from Mission m " + whereClause + " order by location");
         return query.getResultList();
     }
-    
-    public static List<String> getListOfCustomers(String term)
-    {
+
+    public static List<String> getListOfCustomers(String term) {
         String whereClause = "";
-        if (term != null)
-        {
-            whereClause = "where UPPER(customer) like UPPER('%"+term+"%') ";
+        if (term != null) {
+            whereClause = "where UPPER(customer) like UPPER('%" + term + "%') ";
         }
-        Query query = JPA.em().createQuery("select distinct customer from Mission m "+whereClause+" order by customer");
+        Query query = JPA.em().createQuery("select distinct customer from Mission m " + whereClause + " order by customer");
         return query.getResultList();
     }
 }
